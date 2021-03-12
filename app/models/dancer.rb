9bb912afd243da6c1c630582f980f190a9706829
david <calhoun5321@gmail.com>
2021-03-12 13:01:55 -0800

    adds logic to dancer show

--- app/models/dancer.rb
+++ app/models/dancer.rb
@@ -1,5 +1,5 @@
 class Dancer < ApplicationRecord
-  belongs_to :genre, optional: true 
+  belongs_to :genre, optional: true
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :resume, :about, :video, :first_name, :last_name, presence: true, on: :update
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ApplicationRecord
   has_many :posts 
   has_many :dancers 
+  
 end
